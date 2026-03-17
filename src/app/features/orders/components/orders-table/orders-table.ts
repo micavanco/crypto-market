1bb01dc3c73862f813b5ab3c7973b8e757ac9e1a
micavanco <micavanco@gmail.com>
2026-03-17 15:29:09 +0100

    feat(app): add table with mocked data

--- src/app/features/orders/components/orders-table/orders-table.ts
+++ src/app/features/orders/components/orders-table/orders-table.ts
@@ -7,5 +7,33 @@ import { Component } from '@angular/core';
   styleUrl: './orders-table.css',
 })
 export class OrdersTable {
-
+  orders = [
+    {
+      symbol: "BTCUSD",
+      openPriceTotal: 320432.5,
+      sizeTotal: 0.04,
+      swapTotal: -0.008,
+      profitTotal: 11.2102,
+      transactions: [
+        {
+          "openTime": 1750740422000,
+          "openPrice": 104837.47,
+          "swap": -0.00147939,
+          "id": 1203384,
+          "side": "BUY",
+          "size": 0.05,
+          profit: 23.432
+        },
+        {
+          "openTime": 1769870131000,
+          "openPrice": 81536.02,
+          "swap": -6.55e-05,
+          "id": 1226230,
+          "side": "SELL",
+          "size": 0.01,
+          profit: -11.432
+        },
+      ]
+    },
+  ]
 }
